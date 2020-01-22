cf41b4da08dbdfbf03ef9510e35c0c26d71e31d6
Meheret Egzerab <meheretgirma22@gmail.com>
2020-01-21 22:49:51 -0500

    setup webpack and babel

---
+++ src/components/container/WelcomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function WelcomePage(){
+    return (
+        <div classname = 'new'>
+             <h1> Welcome To My Meal Prep</h1>
+        </div>
+    );
+}
+
+export default WelcomePage;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
+/**
+    What this config is doing is for every file with js or jsx extension webpack pipes the code through babel-loader for transformating 
+    into ES6 down to ES5
+*/
